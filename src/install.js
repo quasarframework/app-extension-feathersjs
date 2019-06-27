263a46a34b64cd962f3d715ea8556c168ff0dce1
patcarter883 <33055183+patcarter883@users.noreply.github.com>
2019-06-27 19:16:34 +1000

    Install.js update (#10)
    * Fix install script to install server
    
    * Update .gitignore for vscode

--- src/install.js
+++ src/install.js
@@ -8,9 +8,13 @@
  * @param {function} api
  */
 module.exports = function (api) {
-  api.render('./templates/base')
+  if (api.prompts.options.includes('frontend')) {
+    api.render('./templates/base')
+  }
   /* istanbul ignore if */
-  if (api.prompts.options === 'server') {
+  if (api.prompts.options.includes('server')) {
     api.render('./templates/server')
   }
+
+  api.onExitLog("Update your quasar.conf.js to include boot files for this extension.")
 }
